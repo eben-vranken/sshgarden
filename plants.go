4d8f77e6eee0bec7c53964521e6c56e4846cdc62
eben-vranken <vranken.eben@outlook.com>
2026-06-18 16:19:35 +0200

    feat: create plant registry

--- plants.go
+++ plants.go
@@ -14,3 +14,14 @@ type plant struct {
 	growthRunes      []rune
 	color            lipgloss.Color
 }
+
+var plantRegistry = map[string]*plant{
+	"carrot": {
+		name:             "Carrot",
+		growthTime:       time.Hour * 2,
+		waterRequirement: time.Minute * 30,
+		thirstTolerance:  time.Minute * 30,
+		growthRunes:      []rune{'.', ',', 'i', 'Y'},
+		color:            colorOrange,
+	},
+}
